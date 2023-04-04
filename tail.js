3ae47cdbdbe13d3eec67d7c62cc76ba2e7a47da4
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-04 05:37:37 +0000

    tail

--- tail.js
+++ tail.js
@@ -1,9 +1,9 @@
-const head = function head(arr) {
-  return arr[0];
+const tail = function(arr) {
+  return arr.slice(1);
 };
 const assertEqual = function(actual, expected) {
   console.assert(actual === expected);
 };
-const words = ["Yo-Yo", "Lighthouse", "Labs"];
+const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3); 

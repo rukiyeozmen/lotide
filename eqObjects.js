5288dc5d9402ba427113b1b57983b0e2b50d6276
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-08 04:46:17 +0000

    comparing 2 objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,17 +1,40 @@
-const assertEqual = function(actual, expected) {
+const assertEquals = function(actual, expected) {
   return actual === expected;
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const eqObjects = function(object1, object2) {
-  if (object1 === object2) {
-    return true;
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
   }
-  return false;
+
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
 console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+console.log(assertEquals(eqObjects(shirtObject, anotherShirtObject), true));
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
+assertEquals(eqObjects(shirtObject, longSleeveShirtObject), false);

26cae4900f257d8e34713b409d43b7f24fa69d67
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-10 23:42:45 +0000

    added chai and mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -12,10 +12,11 @@ const { middle } = require('../middle');
 describe('middle', () => {
   it('returns an empty array when given an empty array', () => {
     assert.deepEqual(middle([]), []);
+    assert.deepEqual(middle([1, 2]), []);
+    assert.deepEqual(middle([1]), []);
   });
 
   it('returns the middle element of an array with an odd number of elements', () => {
-    assert.deepEqual(middle([1, 2, 3]), [2]);
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 

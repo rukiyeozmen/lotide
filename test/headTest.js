34eb95d500682410689353b5e99199d721170577
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-10 22:14:34 +0000

    used Mocha and Chai

---
+++ test/headTest.js
@@ -0,0 +1,26 @@
+// const { head, assertEqual } = require('../head');
+
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]));
+
+const assert = require('chai').assert;
+const { head } = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return 'Hello' for ['Hello','Lighthouse','Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("should return undefined for empty []", () => {
+    assert.strictEqual(head([]));
+  });
+});

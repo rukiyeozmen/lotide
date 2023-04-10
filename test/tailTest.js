db30aa0395461d7e0a9553d5b9b56d8576fc3080
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-10 23:10:10 +0000

    added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const {tail,assertEqual} = require('../tail')
+// const {tail,assertEqual} = require('../tail')
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); 
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3); 
+
+const assert = require('chai').assert;
+const { tail } = require('../tail');
+
+describe("#tail", () => {
+
+  it('should return ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+});

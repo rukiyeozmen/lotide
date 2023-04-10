5caf518619c08613c82740a9366f1daa5e0927db
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-10 23:39:55 +0000

    added mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,25 @@
-const { asserArraysEqual, eqArray, middle } = require('../middle');
+// const { asserArraysEqual, eqArray, middle } = require('../middle');
 
 
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2]));
-console.log(middle([1]));
+// console.log(middle([1, 2, 3, 4]));
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2]));
+// console.log(middle([1]));
+
+const assert = require('chai').assert;
+const { middle } = require('../middle');
+
+describe('middle', () => {
+  it('returns an empty array when given an empty array', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it('returns the middle element of an array with an odd number of elements', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('returns the middle two elements of an array with an even number of elements', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});

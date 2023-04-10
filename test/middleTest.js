1b71df1af334cdbd0e38b48257264f726fab8ca7
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-10 20:38:22 +0000

    added test file for middle

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const { asserArraysEqual, eqArray, middle } = require('../middle');
+
+
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2]));
+console.log(middle([1]));
